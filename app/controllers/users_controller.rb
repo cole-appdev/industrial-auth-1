3720e15e6364562ce1747f26622bb8003b94bfe6
johnson-cole <colejohnson@chicagobooth.edu>
2021-04-25 16:21:15 +0000

    completed hardening exercise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show liked feed followers following discover ]
 
+  def feed
+    if params[:username] && params[:username] != current_user.username
+      redirect_back fallback_location: root_url, alert: "You're not authorized for that."
+    end
+  end
+
+  def discover
+    if params[:username] && params[:username] != current_user.username
+      redirect_back fallback_location: root_url, alert: "You're not authorized for that."
+    end
+  end
+
   private
 
     def set_user
